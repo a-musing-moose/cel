11d89c90dce62f67f351723b96a48a768621fbfb
Jonathan Moss <jmoss@snowballone.com.au>
2017-09-23 00:00:44 +1000

    adding templates to list of cli commands to check for

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,5 +2,5 @@ from cel import cli
 
 
 def test_main_has_all_commands_registered():
-    for command in ['build', 'build_runner', 'start', 'run']:
+    for command in ['build', 'build_runner', 'start', 'run', 'templates']:
         assert command in cli.main.commands
